af7d7580761e10779be97010c0c36bac19857ce0
alvinend <alvinendratno@live.com>
2022-12-21 11:41:37 +0900

    Init Mysql Settings

---
+++ src/main/java/com/example/hamsterstask/HamstersTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamsterstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamstersTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamstersTaskApplication.class, args);
+    }
+
+}
